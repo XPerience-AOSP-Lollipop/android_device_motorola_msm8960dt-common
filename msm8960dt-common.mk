7f0f196c9dd45ad050fa46f3d2358d22946a120b
Myles Watson <mylesgw@google.com>
2017-01-13 11:47:17 -0800

    msm8960dt-common:  Build the Bluetooth HAL
    Test: VTS test passes on Marlin, Bluetooth starts/stops
    Change-Id: Ib1a1849c060c39e88c0219cb8421e3426cd3e7ac
    
    Signed-off-by: klozz <shaclone707@gmail.com>

--- msm8960dt-common.mk
+++ msm8960dt-common.mk
@@ -88,6 +88,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8960 \
