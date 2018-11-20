c30ef7aafced5eac381416c12120b9c73ad87ac7
klozz <shaclone707@gmail.com>
2018-11-20 02:00:56 -0500

    fix some libshims
    Signed-off-by: klozz <shaclone707@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -15,83 +15,58 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 # liblog
-
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     moto_log.c
-
 LOCAL_MODULE := libshim_log
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 # libmdmcutback
-
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     moto_mdmcutback.c
-
 LOCAL_MODULE := libshim_mdmcutback
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 # libqc-opt
-
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     icu53.c
-
 LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
 LOCAL_MODULE := libshim_qcopt
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 # libril
-
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     moto_ril.c
-
 LOCAL_SHARED_LIBRARIES := libbinder
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := thermal.c
-
 LOCAL_MODULE := libshims_thermal
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := GraphicBuffer.cpp
-
 LOCAL_MODULE := libshims_graphicbuffer
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 # libshims_cameraservice
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     libcameraservice/CameraFlashlight.cpp \
     libcameraservice/api1/CameraClient.cpp
-
 LOCAL_C_INCLUDES += \
     frameworks/native/include/media/openmax
-
 LOCAL_SHARED_LIBRARIES:= \
     libui \
     liblog \
@@ -111,7 +86,6 @@ LOCAL_SHARED_LIBRARIES:= \
 
 LOCAL_MODULE := libshims_cameraservice
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -131,7 +105,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     sensorlistener/ISensorServer.cpp \
     sensorlistener/SensorManager.cpp
-
 LOCAL_MODULE := libshim_sensorlistener
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := \
@@ -148,8 +121,6 @@ LOCAL_SHARED_LIBRARIES := \
     libgui
 
 include $(BUILD_SHARED_LIBRARY)
-
-
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := camera_server.cpp
